1bfc2465a5e4fbd86517ca59dbcb95eec540d4d8
hosein feizresan <hosein.feiz@gmail.com>
2024-10-22 17:03:46 +0330

    add eslint and prettier

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const router = require('./routes')
 const morgan = require('morgan')
 const DBConnect = require('./configs.js/dbConnect')
 
-app.use(express.json());
+app.use(express.json())
 app.use(morgan('dev'))
-app.use('/' , router)
+app.use('/', router)
 
 app.use((req, res) => {
-    res.status(404).send('Page not found')
-  })
+  res.status(404).send('Page not found')
+})
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-  console.error(err.stack);
+  const statusCode = err.statusCode || 500
+  console.error(err.stack)
   res.status(statusCode).json({
     status: 'error',
     message: err.message || 'Internal Server Error',
-    statusCode: statusCode
-  });
+    statusCode: statusCode,
   })
+})
 
 DBConnect()
 
-app.listen(3000 , ()=>{
-    console.log('server is running')
+app.listen(3000, () => {
+  console.log('server is running')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-
 const LoginController = require('../app/controllers/LoginController')
 
-router.get('/login' , LoginController.get)
-router.post('/login' , LoginController.post)
-
+router.get('/login', LoginController.get)
+router.post('/login', LoginController.post)
 
 module.exports = router
